eedb9ef60397a827a16c9d6681784637fc49afdd
ViniiGama <vinicius.gama@sptech.school>
2026-09-10 13:24:24 -0300

    Retirando componentes

--- projeto-individual-front-end/src/components/Header.jsx
+++ projeto-individual-front-end/src/components/Header.jsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <span className={styles.logoMark} />
+        {/* <span className={styles.logoMark} /> */}
         <span className={styles.logoText}>UNDERGROUND GARAGE</span>
       </div>
-      <div className={styles.statusPill}>
+      {/* <div className={styles.statusPill}>
         <span className={styles.statusDot} />
         GARAGEM ONLINE
-      </div>
+      </div> */}
     </header>
   );
 }
